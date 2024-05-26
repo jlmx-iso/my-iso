bd6e36128577e12f69cea7707fc832cef90f45e8
Jordan Lamoreaux <jnacious88@gmail.com>
2024-05-26 08:50:16 -0500

    more lint

--- src/app/pricing/page.tsx
+++ src/app/pricing/page.tsx
@@ -1,4 +1,4 @@
-import { Container, Group, Space, Table, Title } from "@mantine/core";
+import { Container, Group, Space, Title } from "@mantine/core";
 import PriceCard from "../_components/PriceCard";
 import PageHeading from "../_components/PageHeading";
 import FeatureTable from "../_components/FeatureTable";
@@ -10,27 +10,27 @@ export default function Page() {
       <Title order={4} fw={300} fs="italic" className="text-center">
         Choose a plan that works for you
       </Title>
-        <Group
-          align="center"
-          gap="lg"
-          justify="center"
-          style={{ marginTop: "2rem", marginBottom: "2rem" }}
-          w="100%"
-        >
-          <PriceCard
-            title="Basic"
-            price={0}
-            features={["1 user", "1 project", "1000 requests"]}
-            buttonText="Sign up"
-            buttonVariant="contained"
-          />
-          <PriceCard
-            title="Pro"
-            price={10}
-            features={["5 users", "10 projects", "5000 requests"]}
-            buttonText="Sign up"
-            buttonVariant="outlined"
-          />
+      <Group
+        align="center"
+        gap="lg"
+        justify="center"
+        style={{ marginTop: "2rem", marginBottom: "2rem" }}
+        w="100%"
+      >
+        <PriceCard
+          title="Basic"
+          price={0}
+          features={["1 user", "1 project", "1000 requests"]}
+          buttonText="Sign up"
+          buttonVariant="contained"
+        />
+        <PriceCard
+          title="Pro"
+          price={10}
+          features={["5 users", "10 projects", "5000 requests"]}
+          buttonText="Sign up"
+          buttonVariant="outlined"
+        />
       </Group>
       <Space h="xl" />
       <Container>
