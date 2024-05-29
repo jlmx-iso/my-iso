bae9220f13be3ddc948275ab7ada1de04ecc5211
Jordan Lamoreaux <jnacious88@gmail.com>
2024-05-29 08:28:49 -0500

    remove unused imports

--- src/app/_server_components/NavBar.tsx
+++ src/app/_server_components/NavBar.tsx
@@ -1,10 +1,9 @@
 import React from "react";
 import Link from "next/link";
-import { Avatar, Container, Group } from "@mantine/core";
+import { Container, Group } from "@mantine/core";
 import { getServerAuthSession } from "~/server/auth";
 import Image from "next/image";
 import logo from "../../../public/img/logo.webp"
-import { LoginButton, LogoutButton } from "../_components";
 import { UserProfileButton } from "../_components/buttons/UserProfileButton";
 
 export async function Navbar() {
