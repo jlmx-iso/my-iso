3935f3e256b6966c2f39bd2049183438b467ad87
Iris <iris@myiso.app>
2026-02-26 14:18:17 -0600

    fix: group alias imports before relative imports in keys.ts

--- src/server/api/routers/keys.ts
+++ src/server/api/routers/keys.ts
@@ -2,8 +2,8 @@ import { TRPCError } from "@trpc/server";
 import { z } from "zod";
 
 import { env } from "~/env";
-import { createTRPCRouter, protectedProcedure } from "../trpc";
 import { logger } from "~/_utils";
+import { createTRPCRouter, protectedProcedure } from "../trpc";
 
 const EC_PUBLIC_JWK_SCHEMA = z.record(z.string(), z.unknown()).refine(
   (v) =>
